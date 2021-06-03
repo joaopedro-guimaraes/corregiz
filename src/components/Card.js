1d85ce5a8152ab059fec362165d1068b1f006640
João Pedro Guimarães <joaopedro.enoki@gmail.com>
2021-06-03 12:05:59 -0300

    reset project

--- src/components/Card.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/Card.module.css';
-
-function Card({ children }) {
-  return <div className={styles.card}>{children}</div>;
-}
-
-export default Card;
--- src/components/InputBlock.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/InputBlock.module.css';
-
-function InputBlock({ children }) {
-  return <div className={styles.inputBlock}>{children}</div>;
-}
-
-export default InputBlock;
